daa7631520b402e77f2912094d9f907d6c4c4b35
323908863 <323908863@C-3ZZHYR1.tdsb.on.ca>
2016-09-14 13:21:56 -0400

    Uploading part 1 of About me

---
+++ AboutMe/src/barre/abouMe.java
@@ -0,0 +1,20 @@
+package barre;
+/*aboutMe.java
+ * 
+ *created September 14th 2016
+ *print out information
+ *abdirahman barre
+ */ 
+
+public class abouMe {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Abdirahman.B");
+		System.out.println("K.Kemp");
+		System.out.println("Runnymede C.I");
+		System.out.println("\"Go Ravens!\"");
+		
+	}
+
+}
