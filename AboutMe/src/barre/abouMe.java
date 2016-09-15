b2e7e39c766f80a54582cea5e59d6338aacd0ab8
323908863 <323908863@C-3ZZHYR1.tdsb.on.ca>
2016-09-15 12:56:27 -0400

    Part2

--- AboutMe/src/barre/abouMe.java
+++ AboutMe/src/barre/abouMe.java
@@ -15,6 +15,11 @@ public class abouMe {
 		System.out.println("Runnymede C.I");
 		System.out.println("\"Go Ravens!\"");
 		
+		System.out.format("%-10s %8s %18s","Weekdays,\t","Class Code,\t","Class Start Times \n");
+		System.out.format("%-10s %8s %18s","Monday-Friday,\t","3U1-01\t","08:45am\n");
+		System.out.format("%-10s %8s %18s","Monday-Friday,\t","SCH3U1-01\t","08:45am\n");
+		System.out.format("%-10s %8s %18s","Monday-Friday,\t","ICS3U!-01\t","08:45am\n");
+		System.out.format("%-10s %8s %18s","MOnday-Friday,\t","PPL3OM-01\t","08:45am\n");
 	}
 
 }
