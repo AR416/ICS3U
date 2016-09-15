2d4bf305de1f56f18c3d3d71182f205d95b46a0f
323908863 <323908863@C-3ZZHYR1.tdsb.on.ca>
2016-09-15 13:41:45 -0400

    Ex-1 Bingo

---
+++ AboutMe/src/barre/BingoCard.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package barre;
+
+/*bingoCard.java
+ * 
+ *created September 15th 2016
+ *generate a bingo card
+ *abdirahman barre
+ */
+public class BingoCard {
+
+	public static void main(String[] args) {
+		
+		System.out.format("%5s %5s %5s %5s %5s","B","I","N","G","O\n");
+		System.out.format("%5s %5s %5s %5s %5s","3","15","40","55","71\n");	
+		System.out.format("%5s %5s %5s %5s %5s","8","23","33","47","67\n");
+		System.out.format("%5s %5s %6s %4s %5s","4","18","FREE","59","64\n");
+		System.out.format("%5s %5s %5s %5s %5s","12","29","39","50","70\n");
+		System.out.format("%5s %5s %5s %5s %5s","2","21","30","60","75\n");
+		
+				
+			
+	}
+
+}
