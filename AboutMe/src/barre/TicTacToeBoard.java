63954d546c5a5740cbebe16c50bb2dbdb09f8e2f
323908863 <323908863@C-3ZZHYR1.tdsb.on.ca>
2016-09-19 12:56:49 -0400

    TicTacToeBoard

---
+++ AboutMe/src/barre/TicTacToeBoard.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package barre;
+
+/*tictactoeBoard.java
+ * 
+ *created September 19th 2016
+ *generate a tictactoe board
+ *abdirahman barre
+ */
+
+public class TicTacToeBoard {
+
+	
+	public static void main(String[] args) {
+	
+		System.out.println("\t|\t|");
+	    System.out.println("\t|\t|");
+		System.out.println("\t|\t|");
+		System.out.println("  --------------------");
+		System.out.println("\t|\t|");
+		System.out.println("\t|   X  \t|");
+		System.out.println("\t|\t|");
+		System.out.println("  --------------------");
+		System.out.println("\t|\t|");
+		System.out.println("\t|\t|");
+		System.out.println("\t|\t|");
+
+	}
+
+}
