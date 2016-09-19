4b2e7af57f643a609a6d16bd1530d4a3a71692b0
323908863 <323908863@C-3ZZHYR1.tdsb.on.ca>
2016-09-19 13:11:41 -0400

    Tic Tac Toe Rules

---
+++ AboutMe/src/barre/TicTacToeRules.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package barre;
+
+/*TicTacToeRules.java
+ * 
+ *created September 19th 2016
+ *generate rules for TicTacToe
+ *abdirahman barre
+ */
+ 
+public class TicTacToeRules {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Tic Tac Toe Rules\n");
+		System.out.println("Tic Tac Toe is a game for 2 players.\n");
+		System.out.println("1. A grid of 9 squares is drawn. \n");
+		System.out.println("2. the first player drwas an X in a empty square.\n");
+		System.out.println("3. The second player draws an O in a empty square. \n");
+		System.out.println("4. Step 2 and 3 are repeated until a Tic Tac pattern is created.\n");
+		System.out.println("  Patterns can be diagonal, horizontal, or vertical.\n");
+		System.out.println("5. A winning pattern is completed by drawing a line through the pattern.\n");
+	}
+
+}
