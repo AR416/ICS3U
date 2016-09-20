9e8bc57d0b41a9dd4823f58c3ab6f5b61704514b
323908863 <323908863@C-3ZZHYR1.tdsb.on.ca>
2016-09-20 13:04:49 -0400

    BingoRules

---
+++ AboutMe/src/barre/BingoRules.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package barre;
+
+/*BingoRules
+ * 
+ *created September 20th 2016
+ *generate rules for a bingo game
+ *abdirahman barre 
+ */
+public class BingoRules {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Bingo Card\n");
+		System.out.println("1. The caller randomly pulls a numbered bingo ball.\n");
+		System.out.println("2. The number is placed on the bingo board and called out. \n");
+		System.out.println("3. players look for the called number on their bingo card.\n");
+		System.out.println("4. If the number is located, it is marked off.\n");
+		System.out.println("5. Step 1 to 4 are repeated until a player matches the bingo pattern.\n");
+		System.out.println("6. The winning player yells BINGO.\n");
+		
+	}
+
+
+	}
+
+
