f5aecf598300f193fef66ab897312e98d8d52378
323908863 <323908863@C-3ZZHYR1.tdsb.on.ca>
2016-09-20 12:54:16 -0400

    BinaryNumbers

---
+++ AboutMe/src/barre/BinaryNumbers.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package barre;
+
+/*binaryNumbers.java
+ * 
+ *created September 15th 2016
+ *generate a bingo card
+ *abdirahman barre
+ */
+public class BinaryNumbers {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Decimal\t    Binary      Hexadecimal\n");
+		System.out.println("1\t    1           1\n");
+		System.out.println("2\t    10          2\n");
+		System.out.println("3\t    11          3\n");
+		System.out.println("4\t    100         4\n");
+		System.out.println("5\t    101         5\n");
+		System.out.println("6\t    110         6\n");
+		System.out.println("7\t    111         7\n");
+		System.out.println("8\t    1000        8\n");
+		System.out.println("9\t    1001        9\n");
+		System.out.println("10\t    1010        A\n");
+		System.out.println("11\t    1011        B\n");
+		System.out.println("12\t    1100        C\n");
+		System.out.println("13\t    1101        D\n");
+		System.out.println("14\t    1110        E\n");
+		System.out.println("15\t    1111        F\n");
+		System.out.println("16\t    10000       10\n");
+		System.out.println("17\t    10001       11\n");
+		System.out.println("18\t    10010       12\n");
+		System.out.println("19\t    10011       13\n");
+		System.out.println("20\t    10100       14\n");
+		
+	}
+
+}
